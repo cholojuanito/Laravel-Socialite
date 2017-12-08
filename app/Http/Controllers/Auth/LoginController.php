f197ea4c66b419d276ad05827eef89cf3d2aa7ef
cholojuanito <tannerdavisr@gmail.com>
2017-12-08 13:10:48 -0700

    Initial commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+   /**
+    * Handle Social login request from one of the social media options
+    * Redirects the user to the corresponding authentication page for
+    * the social media app they chose.
+    * @return response
+    */
+
+   public function socialLogin($social)
+   {
+       return Socialite::driver($social)->redirect();
+   }
+
+   /**
+    * Obtain the user information from Social Logged in.
+    * @param $social
+    * @return Response
+    */
+
+   public function handleProviderCallback($social)
+   {
+       $userSocial = Socialite::driver($social)->user();
+       $user = User::where(['email' => $userSocial->getEmail()])->first();
+
+       if($user) {
+           Auth::login($user);
+           return redirect()->action('HomeController@index');
+
+       }
+       else {
+           return view('auth.register', [
+             'name' => $userSocial->getName(),
+             'email' => $userSocial->getEmail()
+           ]);
+       }
+   }
+}
